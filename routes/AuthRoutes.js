ae738820f062a769a126650504499ac73666cb3b
Pratibha <ionictest2021@gmail.com>
2025-07-24 13:26:29 +0530

    added changes

--- routes/AuthRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { SignIn, SignUp } from '../controllers/AuthController.js';
-const router=express.Router();
-router.post("/signin",SignIn);
-router.post("/signup",SignUp);
-export default router;
